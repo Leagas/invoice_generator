fb0219fddfd86e07888929c33ce46676704fd221
Lloyd <lloydleagas@gmail.com>
2021-08-19 07:06:13 +0200

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+const path = require("path")
+const data = require("./input.json")
+
+async function main(data) {
+
+  for (let i = 0; i < data.length; i++) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    await page.addScriptTag({ content: `window.name = "${data[i].name}"` });
+    await page.goto(require.resolve(path.join(__dirname, "./index.html")));
+    await page.pdf({ path: `output/${i}.pdf` })
+  
+    await browser.close();
+  }
+}
+
+main(data)
