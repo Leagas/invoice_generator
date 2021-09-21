c02df11970b449cd94ecaf396211415a77a188f8
Lloyd <lloydleagas@gmail.com>
2021-09-21 10:13:47 +0200

    Update

--- main.js
+++ main.js
@@ -2,16 +2,41 @@ const puppeteer = require('puppeteer');
 const path = require("path")
 const data = require("./input.json")
 
+function getContent(i) {
+  const keys = Object.keys(data[i]);
+
+  return keys.reduce((prev, curr, index) => {
+    let currentItem = data[i][curr];
+    let  newItem;
+
+    if (typeof currentItem !== "string") {
+      currentItem = JSON.stringify(currentItem)
+      newItem = `let ${curr} = ${currentItem}`
+    } else {
+      newItem = `let ${curr} = '${currentItem}'`
+    }
+
+    if (index !== keys.length) {
+      prev += "\n"
+    }
+
+    return prev + newItem
+  }, "")
+}
+
 async function main(data) {
 
   for (let i = 0; i < data.length; i++) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-  
-    await page.addScriptTag({ content: `window.name = "${data[i].name}"` });
-    await page.goto(require.resolve(path.join(__dirname, "./index.html")));
-    await page.pdf({ path: `output/${i}.pdf`, format: "a4" })
-  
+
+    await page.goto(require.resolve(path.join(__dirname, "./index.html")), {
+      waitUntil: 'networkidle0',
+      timeout: 0
+    });
+    await page.addScriptTag({type: "text/javascript", content: `${getContent(i)}; init();` });
+    await page.pdf({ path: `output/${i}.pdf` })
+
     await browser.close();
   }
 }
